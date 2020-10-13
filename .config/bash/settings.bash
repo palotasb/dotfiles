2f28494b373aecceb898499eabffcd068a35b2a8
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2020-10-13 17:59:40 +0200

    Add default ubuntu bashrc settings

--- .config/bash/settings.bash
+++ .config/bash/settings.bash
@@ -1,14 +1,39 @@
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1048576
+HISTFILESIZE=1048576
 HISTFILE="$HOME/.bash_history"
 SAVEHIST=$HISTSIZE
-
-shopt -s histappend # append to history file
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
 
-# Completion
-# brew install bash-completion
-# [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  elif [ -f  /usr/local/etc/bash_completion ]; then  # brew install bash-completion
+    . /etc/bash_completion
+  fi
+fi
 
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+fi
